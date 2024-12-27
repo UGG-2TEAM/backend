01d227fd4f6b3fc3187e450d6289e4818d1c2e1f
Kim So Yeon <qws1566@naver.com>
2024-12-27 17:39:01 +0900

    initial

---
+++ src/main/java/org/example/ugg/UggApplication.java
@@ -0,0 +1,13 @@
+package org.example.ugg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ugg/UggApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ugg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
